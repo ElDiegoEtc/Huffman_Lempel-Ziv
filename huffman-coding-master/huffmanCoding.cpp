5e4e7ee133e4a7b85e5025c267b58458316bf3ea
Diego Oyarzo <diegoyarzonavia@gmail.com>
2024-06-16 18:16:42 -0400

    Guardado csv huffman y mas ejemplos de textos

--- huffman-coding-master/huffmanCoding.cpp
+++ huffman-coding-master/huffmanCoding.cpp
@@ -1,33 +1,68 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
 #include "huffman.h"
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		cout << "Usage:\n\t huffmanCoding inputfile outputfile" << endl;
-		exit(1);
-	}
-	float tiempo_total = 0;
+    if (argc != 3)
+    {
+        cout << "Usage:\n\t huffmanCoding inputfile outputfile" << endl;
+        exit(1);
+    }
+    float tiempo_total = 0;
 
-	for (int i = 1; i <= 20; i++)
-	{
-		auto start = std::chrono::high_resolution_clock::now();
-		huffman h(argv[1], argv[2]);
-		h.create_pq();
-		h.create_huffman_tree();
-		h.calculate_huffman_codes();
-		if (i == 20){
-			h.coding_save();
-		}
-		auto end = std::chrono::high_resolution_clock::now();
-		std::chrono::nanoseconds elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
-		tiempo_total += elapsed.count();
-	}
-	double avg_time = tiempo_total/20;
-	cout << "Tiempo promedio (20 iteraciones): " << avg_time << "ns";
-	cout << endl;
-	return 0;
+    for (int i = 1; i <= 20; i++)
+    {
+        auto start = std::chrono::high_resolution_clock::now();
+        huffman h(argv[1], argv[2]);
+        h.create_pq();
+        h.create_huffman_tree();
+        h.calculate_huffman_codes();
+        if (i == 20){
+            h.coding_save();
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::microseconds elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+        tiempo_total += elapsed.count();
+    }
+    double avg_time = tiempo_total / 20;
+
+    // Calcula tamaño de archivo input y output
+    ifstream input_file(argv[1], ios::binary | ios::ate);
+    ifstream output_file(argv[2], ios::binary | ios::ate);
+
+    if (!input_file.is_open()) {
+        cerr << "Error opening input file to calculate size" << endl;
+        return 1;
+    }
+    if (!output_file.is_open()) {
+        cerr << "Error opening output file to calculate size" << endl;
+        return 1;
+    }
+
+    auto input_size = input_file.tellg();
+    auto output_size = output_file.tellg();
+
+    input_file.close();
+    output_file.close();
+
+    // guarda avg_time, input_size, and output_size en archivo CSV
+    ofstream csv_file("resultados_coding.csv", ios::app); // Abre en append mode para no sobreescribir datos anteriores
+    if (!csv_file.is_open()) {
+        cerr << "Error opening resultados_coding.csv file" << endl;
+        return 1;
+    }
+
+    // If the file is newly created, add headers
+    csv_file.seekp(0, ios::end);
+    if (csv_file.tellp() == 0) {
+        csv_file << "Tiempo promedio(ms),Archivo Original(bytes),Archivo Codificado(bytes)" << endl;
+    }
+
+    csv_file << avg_time << "," << input_size << "," << output_size << endl;
+    csv_file.close();
+
+    return 0;
 }
--- huffman-coding-master/huffmanDecoding.cpp
+++ huffman-coding-master/huffmanDecoding.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <fstream>
 #include "huffman.h"
 using namespace std;
 
@@ -25,7 +26,42 @@ int main(int argc, char *argv[])
 		tiempo_total += elapsed.count();
 	}
 	float avg_time = tiempo_total/20;
-	cout << "Tiempo promedio (20 iteraciones): " << avg_time << "ns";
 	cout << endl;
-	return 0;
+
+	// Calcula tamaño de archivo input y output
+    ifstream input_file(argv[1], ios::binary | ios::ate);
+    ifstream output_file(argv[2], ios::binary | ios::ate);
+
+    if (!input_file.is_open()) {
+        cerr << "Error opening input file to calculate size" << endl;
+        return 1;
+    }
+    if (!output_file.is_open()) {
+        cerr << "Error opening output file to calculate size" << endl;
+        return 1;
+    }
+
+    auto input_size = input_file.tellg();
+    auto output_size = output_file.tellg();
+
+    input_file.close();
+    output_file.close();
+
+    // guarda avg_time, input_size, and output_size en archivo CSV
+    ofstream csv_file("resultados_decoding.csv", ios::app); // Abre en append mode para no sobreescribir datos anteriores
+    if (!csv_file.is_open()) {
+        cerr << "Error opening resultados_decoding.csv file" << endl;
+        return 1;
+    }
+
+    // If the file is newly created, add headers
+    csv_file.seekp(0, ios::end);
+    if (csv_file.tellp() == 0) {
+        csv_file << "Tiempo promedio(ms),Archivo Codificado(bytes),Archivo Decodificado(bytes)" << endl;
+    }
+
+    csv_file << avg_time << "," << input_size << "," << output_size << endl;
+    csv_file.close();
+
+    return 0;
 }
